4ebb634a7eb3c5fd92de6ca48cd71bfa05051a7c
debug-version <25eg6nomemg3@mail.ru>
2025-04-19 22:59:15 +0400

    feat(pages): initialize Razor Pages infrastructure

---
+++ lab-02-razor-pages/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
